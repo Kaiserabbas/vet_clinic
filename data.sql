0297b451d5c3ac7298bac8e2cc5f13c0368f48f5
Qaisar Abbas <kayser.abbas@gmail.com>
2023-09-26 12:25:44 +0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -19,11 +19,11 @@ VALUES ('Devimon', '2017-05-12', 11, true, 5);
 
 -- Adding new animals after the addition os species column
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) VALUES
-('Charmander', '2020-02-08', -11, false, 0, 'Lizard'),
-('Plantmon', '2021-11-15', -5.7, true, 2, 'Dog'),
-('Squirtle', '1993-04-02', -12.13, false, 3, 'Turtle'),
-('Angemon', '2005-06-12', -45, true, 1, 'Cat'),
-('Boarmon', '2005-06-07', 20.4, true, 7, 'Boar'),
-('Blossom', '1998-10-13', 17, true, 3, 'Rabbit'),
-('Ditto', '2022-05-14', 22, true, 4, 'Cat');
+('Charmander', '2020-02-08', -11, false, 0'),
+('Plantmon', '2021-11-15', -5.7, true, 2),
+('Squirtle', '1993-04-02', -12.13, false, 3),
+('Angemon', '2005-06-12', -45, true, 1),
+('Boarmon', '2005-06-07', 20.4, true, 7),
+('Blossom', '1998-10-13', 17, true, 3),
+('Ditto', '2022-05-14', 22, true, 4);
 
