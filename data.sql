308be2dbde93a77bd4ad7bd21fd5b84836b27f87
Qaisar Abbas <kayser.abbas@gmail.com>
2023-09-26 08:53:13 +0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,17 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+-- Insert data for Agumon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
+
+-- Insert data for Gabumon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Gabumon', '2018-11-15', 8, true, 2);
+
+-- Insert data for Pikachu
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
+
+-- Insert data for Devimon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Devimon', '2017-05-12', 11, true, 5);
