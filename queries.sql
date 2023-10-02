4d08fd08c277d97950126406845fe34c93231879
tajemouti <tajemouti@gmail.com>
2023-10-02 19:24:06 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -181,6 +181,10 @@ WHERE vt.name = 'Maisy Smith'
 GROUP BY s.name
 ORDER BY visit_count DESC
 LIMIT 1;
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
  
 
 
