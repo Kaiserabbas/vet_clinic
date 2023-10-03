29ad7e8c30ea1637495195c1818c381860d7dfaf
Qaisar Abbas <kayser.abbas@gmail.com>
2023-10-03 07:32:11 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -182,9 +182,10 @@ GROUP BY s.name
 ORDER BY visit_count DESC
 LIMIT 1;
 
-SELECT COUNT(*) FROM visits where animal_id = 4;
-SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+-- The following queries are to check whether it is taking too much time (1 sec = 1000ms can be considered as too much time for database query).
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
  
 
 
